5aae3a096d2cf60dabccf86724317a78afb040c5
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2021-11-17 23:58:07 +0300

    feat: add example

---
+++ example/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:card_scanner/card_scanner.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var card = ScannedCardModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(card.toString()),
+            _buildButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        await _scanCard();
+      },
+      child: const Text('Scan card'),
+    );
+  }
+
+  Future<void> _scanCard() async {
+    final scanOptions = ScanOptions();
+    final receivedCard = await CardScanner.scanCard(scanOptions: scanOptions);
+    if (receivedCard == null) return;
+    if (!mounted) return;
+    card = receivedCard;
+    setState(() {});
+  }
+}
